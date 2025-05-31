e9fd77bcc2f6d3378543e2c1b0a72018560d3649
FlyingTadpole <caogang530@qq.com>
2025-06-01 01:13:40 +0800

    update

--- tests/test_account.py
+++ tests/test_account.py
@@ -15,8 +15,8 @@ class TestLogin:
 
     @pytest.mark.parametrize("email, password",[
         ("test1@mail.com", 123456),
-        # ("test2@mail.com", 123456),
-        # ("test3@mail.com", 123456)
+        ("test2@mail.com", 123456),
+        ("test3@mail.com", 123456)
     ])
     @allure.story("用户登录成功")
     def test_register_acount1(self, get_driver, email, password):
@@ -36,75 +36,75 @@ class TestLogin:
         # 断言后台清理注册账号成功
         Check(get_driver).check_element_exists((By.XPATH, f'//td[text()="{email}"]'), False)
 
-    # @pytest.mark.parametrize("email, password",[
-    #     ("test4@mail.com", 123456),
-    #     ("test5@mail.com", 123456),
-    #     ("test6@mail.com", 123456)
-    # ])
-    # @allure.story("用户登录成功")
-    # def test_register_acount2(self, get_driver, email, password):
-    #     # 注册账号
-    #     user = UserEngine(get_driver)
-    #     user.register_account(email, password)
-    #
-    #     # 断言 注册成功
-    #     Check(get_driver).check_element_exists((By.LINK_TEXT, "退出登录"), True)
-    #
-    #     # 清理注册的账号
-    #     admin = AdminEngine(get_driver)
-    #     admin.login()
-    #     admin.delete_customer(email)
-    #     time.sleep(1)
-    #
-    #     # 断言后台清理注册账号成功
-    #     Check(get_driver).check_element_exists((By.XPATH, f'//td[text()="{email}"]'), False)
-    #
-    # @pytest.mark.run(order=1)
-    # @pytest.mark.parametrize("email, password",[
-    #     ("test7@mail.com", 123456),
-    #     ("test8@mail.com", 123456),
-    #     ("test9@mail.com", 123456)
-    # ])
-    # @allure.story("用户登录成功")
-    # def test_register_acount3(self, get_driver, email, password):
-    #     # 注册账号
-    #     user = UserEngine(get_driver)
-    #     user.register_account(email, password)
-    #
-    #     # 断言 注册成功
-    #     Check(get_driver).check_element_exists((By.LINK_TEXT, "退出登录"), True)
-    #
-    #     # 清理注册的账号
-    #     admin = AdminEngine(get_driver)
-    #     admin.login()
-    #     admin.delete_customer(email)
-    #     time.sleep(1)
-    #
-    #     # 断言后台清理注册账号成功
-    #     Check(get_driver).check_element_exists((By.XPATH, f'//td[text()="{email}"]'), False)
-    #
-    # @pytest.mark.parametrize("email, password",[
-    #     ("test10@mail.com", 123456),
-    #     ("test11@mail.com", 123456),
-    #     ("test12@mail.com", 123456)
-    # ])
-    # @allure.story("用户登录成功")
-    # def test_register_acount4(self, get_driver, email, password):
-    #     # 注册账号
-    #     user = UserEngine(get_driver)
-    #     user.register_account(email, password)
-    #
-    #     # 断言 注册成功
-    #     Check(get_driver).check_element_exists((By.LINK_TEXT, "退出登录"), True)
-    #
-    #     # 清理注册的账号
-    #     admin = AdminEngine(get_driver)
-    #     admin.login()
-    #     admin.delete_customer(email)
-    #     time.sleep(1)
-    #
-    #     # 断言后台清理注册账号成功
-    #     Check(get_driver).check_element_exists((By.XPATH, f'//td[text()="{email}"]'), False)
+    @pytest.mark.parametrize("email, password",[
+        ("test4@mail.com", 123456),
+        ("test5@mail.com", 123456),
+        ("test6@mail.com", 123456)
+    ])
+    @allure.story("用户登录成功")
+    def test_register_acount2(self, get_driver, email, password):
+        # 注册账号
+        user = UserEngine(get_driver)
+        user.register_account(email, password)
+
+        # 断言 注册成功
+        Check(get_driver).check_element_exists((By.LINK_TEXT, "退出登录"), True)
+
+        # 清理注册的账号
+        admin = AdminEngine(get_driver)
+        admin.login()
+        admin.delete_customer(email)
+        time.sleep(1)
+
+        # 断言后台清理注册账号成功
+        Check(get_driver).check_element_exists((By.XPATH, f'//td[text()="{email}"]'), False)
+
+    @pytest.mark.run(order=1)
+    @pytest.mark.parametrize("email, password",[
+        ("test7@mail.com", 123456),
+        ("test8@mail.com", 123456),
+        ("test9@mail.com", 123456)
+    ])
+    @allure.story("用户登录成功")
+    def test_register_acount3(self, get_driver, email, password):
+        # 注册账号
+        user = UserEngine(get_driver)
+        user.register_account(email, password)
+
+        # 断言 注册成功
+        Check(get_driver).check_element_exists((By.LINK_TEXT, "退出登录"), True)
+
+        # 清理注册的账号
+        admin = AdminEngine(get_driver)
+        admin.login()
+        admin.delete_customer(email)
+        time.sleep(1)
+
+        # 断言后台清理注册账号成功
+        Check(get_driver).check_element_exists((By.XPATH, f'//td[text()="{email}"]'), False)
+
+    @pytest.mark.parametrize("email, password",[
+        ("test10@mail.com", 123456),
+        ("test11@mail.com", 123456),
+        ("test12@mail.com", 123456)
+    ])
+    @allure.story("用户登录成功")
+    def test_register_acount4(self, get_driver, email, password):
+        # 注册账号
+        user = UserEngine(get_driver)
+        user.register_account(email, password)
+
+        # 断言 注册成功
+        Check(get_driver).check_element_exists((By.LINK_TEXT, "退出登录"), True)
+
+        # 清理注册的账号
+        admin = AdminEngine(get_driver)
+        admin.login()
+        admin.delete_customer(email)
+        time.sleep(1)
+
+        # 断言后台清理注册账号成功
+        Check(get_driver).check_element_exists((By.XPATH, f'//td[text()="{email}"]'), False)
 
 if __name__ == '__main__':
     pytest.main(['-sv', __file__, '-n', '4'])
